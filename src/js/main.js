752fca0562f912ef1297c809dab7ef4c01390e7d
Ivan Mikerin <vmik7@yandex.ru>
2020-10-09 13:53:03 +0300

    Commented unused js modules in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,21 @@
 
 // JS function to test webp support
 
-@@include('files/webp-test.js', {})
+// include('files/webp-test.js', {})
 
 // Dynamic Adapt (https://www.youtube.com/watch?v=QKuMr575vlQ)
 
-@@include('libs/dynamic_adapt/da.js', {})
+// include('libs/dynamic_adapt/da.js', {})
 
 // Focus-visible (https://www.npmjs.com/package/focus-visible)
 
-@@include('../../node_modules/focus-visible/dist/focus-visible.js', {})
+@@include('../../node_modules/focus-visible/dist/focus-visible.min.js', {})
 
 // Blocking-elements (https://github.com/PolymerLabs/blocking-elements)
 
-@@include('../../node_modules/babel-polyfill/dist/polyfill.js', {})
-@@include('../../node_modules/wicg-inert/dist/inert.js', {})
-@@include('../../node_modules/blocking-elements/dist/blocking-elements.js', {})
+@@include('../../node_modules/babel-polyfill/dist/polyfill.min.js', {})
+@@include('../../node_modules/wicg-inert/dist/inert.min.js', {})
+@@include('../../node_modules/blocking-elements/dist/blocking-elements.min.js', {})
 
 // Other scripts
 
