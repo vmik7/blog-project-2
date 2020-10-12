0bf578792b8db3219eab1e58e25905e64506315d
Ivan Mikerin <vmik7@yandex.ru>
2020-10-12 17:07:27 +0300

    Первая версия сайдбара (нуждается в доработках)

--- dist/js/main.js
+++ dist/js/main.js
@@ -69,15 +69,48 @@ function undoBlocking(element) {
 
 
 
+// Hero slider
+
+$('.hero__slider').slick({
+    dots: true,
+    infinite: true,
+    initialSlide: 1,
+    speed: 500,
+    fade: true,
+    adaptiveHeight: true,
+    cssEase: 'ease',
+
+    prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><svg height="44" viewBox="0 0 24 44" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 21.9996L19.821 43.0996L24 38.6505L8.358 21.9996L24 5.34869L19.821 0.899609L0 21.9996Z" fill="white"/></svg></button>',
+    nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button" style=""><svg height="44" viewBox="0 0 24 44" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 21.9996L19.821 43.0996L24 38.6505L8.358 21.9996L24 5.34869L19.821 0.899609L0 21.9996Z" fill="white"/></svg></button>',
+});
+
+
+
+// Page elements to controll
+
+const pageTopHeader = document.querySelector('.page__top-header');
+const pageBody = document.querySelector('.page__body');
+const pageMain = document.querySelector('.page__main');
+const pageSidebar = document.querySelector('.page__sidebar');
+const pageContent = document.querySelector('.page__content');
+
+const mainTopHeader = document.querySelector('.main-top-header');
+
+const menuButton = document.querySelector('.main-top-header__menu .menu__button');
+const menuBody = document.querySelector('.main-top-header__menu .menu__body');
+
+const userBlockBody = document.querySelector('.main-top-header .user-block__body');
+const userBlockButton = document.querySelector('.main-top-header .user-block__button');
+
+const sidebarControlButton = document.querySelector('.sidebar__control-btn');
+const sidebarFixedBar = document.querySelector('.sidebar__fixed-bar');
+const sidebarContent = document.querySelector('.sidebar__content');
+
+
 
 // Burger interactive
 
 if (window.matchMedia('(max-width: 768px)').matches) {
-    const pageBody = document.querySelector('.page__body');
-    const mainTopHeader = document.querySelector('.main-top-header');
-    const menuButton = document.querySelector('.main-top-header__menu .menu__button');
-    const menuBody = document.querySelector('.main-top-header__menu .menu__body');
-
     let showNavMenu = () => {
         menuButton.setAttribute('aria-expanded', true)
         menuButton.classList.add('menu__button_active');
@@ -86,7 +119,6 @@ if (window.matchMedia('(max-width: 768px)').matches) {
         pageBody.classList.add('page__body_lock');
         makeBlocking(mainTopHeader);
     };
-
     let hideNavMenu = () => {
         menuButton.setAttribute('aria-expanded', false)
         menuButton.classList.remove('menu__button_active');
@@ -115,11 +147,6 @@ if (window.matchMedia('(max-width: 768px)').matches) {
 // Login button interactive
 
 if (window.matchMedia('(max-width: 768px)').matches) {
-    const pageBody = document.querySelector('.page__body');
-    const mainTopHeader = document.querySelector('.main-top-header');
-    const userBlockBody = document.querySelector('.main-top-header .user-block__body');
-    const userBlockButton = document.querySelector('.main-top-header .user-block__button');
-
     let showLogin = () => {
         userBlockButton.setAttribute('aria-expanded', true)
         userBlockButton.classList.add('user-block__button_active');
@@ -128,7 +155,6 @@ if (window.matchMedia('(max-width: 768px)').matches) {
         pageBody.classList.add('page__body_lock');
         makeBlocking(mainTopHeader);
     };
-
     let hideLogin = () => {
         userBlockButton.setAttribute('aria-expanded', false)
         userBlockButton.classList.remove('user-block__button_active');
@@ -154,35 +180,11 @@ if (window.matchMedia('(max-width: 768px)').matches) {
 
 
 
-// Hero slider
-
-$('.hero__slider').slick({
-    dots: true,
-    infinite: true,
-    initialSlide: 1,
-    speed: 500,
-    fade: true,
-    adaptiveHeight: true,
-    cssEase: 'ease',
-
-    prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><svg height="44" viewBox="0 0 24 44" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 21.9996L19.821 43.0996L24 38.6505L8.358 21.9996L24 5.34869L19.821 0.899609L0 21.9996Z" fill="white"/></svg></button>',
-    nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button" style=""><svg height="44" viewBox="0 0 24 44" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 21.9996L19.821 43.0996L24 38.6505L8.358 21.9996L24 5.34869L19.821 0.899609L0 21.9996Z" fill="white"/></svg></button>',
-});
-
-
-
-// Page interactive (menu, fixed header, ...)
-
-const pageTopHeader = document.querySelector('.page__top-header');
-const pageMain = document.querySelector('.page__main');
-const pageContent = document.querySelector('.page__content');
-const mainTopHeader = document.querySelector('.main-top-header');
-
-
 // Page-main padding 
-pageMain.style.paddingTop = `${mainTopHeader.offsetHeight}px`;
+
+pageBody.style.paddingTop = `${mainTopHeader.offsetHeight}px`;
 window.addEventListener('resize', () => {
-    pageMain.style.paddingTop = `${mainTopHeader.offsetHeight}px`;
+    pageBody.style.paddingTop = `${mainTopHeader.offsetHeight}px`;
 });
 
 // Header scrolling
@@ -192,13 +194,54 @@ window.addEventListener('scroll', () => {
     if (pageYOffset > prevScroll) {
         if (pageYOffset > mainTopHeader.offsetHeight) {
             pageTopHeader.style.top = `-${mainTopHeader.offsetHeight}px`;
+            pageTopHeader.classList.add('top-header_hide');
         }
     }
     else {
         pageTopHeader.style.top = `-${0}px`;
+        pageTopHeader.classList.remove('top-header_hide');
     }
 
     prevScroll = pageYOffset;
 });
 
+
+
 // Sidebar interactive
+
+// Fixing
+let fixSidebar = () => {
+    pageSidebar.style.top = `${Math.max(pageMain.offsetTop - pageYOffset, 0)}px`;
+    pageSidebar.style.paddingTop = `${pageTopHeader.offsetHeight + pageTopHeader.offsetTop}px`;
+}; fixSidebar();
+window.addEventListener('scroll', fixSidebar);
+
+// Hide / Show
+let showSidebar = () => {
+    sidebarControlButton.setAttribute('aria-expanded', true)
+    sidebarControlButton.classList.add('sidebar__control-btn_active');
+    pageSidebar.classList.remove('sidebar_hide');
+    pageSidebar.style.right = `${0}px`;
+    pageMain.style.paddingRight = `${pageSidebar.offsetWidth}px`;
+};
+
+let hideSidebar = () => {
+    sidebarControlButton.setAttribute('aria-expanded', false)
+    sidebarControlButton.classList.remove('sidebar__control-btn_active');
+    pageSidebar.classList.add('sidebar_hide');
+    pageSidebar.style.right = `${-sidebarContent.offsetWidth}px`;
+    pageMain.style.paddingRight = `${sidebarFixedBar.offsetWidth}px`;
+};
+
+pageSidebar.addEventListener('click', () => {
+    let expanded = sidebarControlButton.getAttribute('aria-expanded') === 'true' || false;
+
+    if (!expanded) {
+        showSidebar();
+    }
+    else {
+        hideSidebar();
+    }
+});
+
+showSidebar();
